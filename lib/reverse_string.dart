1251c82bafd32d43e900ea604b380d0b3a9f0384
Muhammed Elsayed <133232940+Mo123elsayed@users.noreply.github.com>
2025-05-27 14:16:52 +0300

    itial commit

--- lib/reverse_string.dart
+++ lib/reverse_string.dart
@@ -1,12 +1,27 @@
 import 'dart:io';
 
-
-void reverseString(String input) {
-  String reversed = input.split('').reversed.join('');
-  print("Reversed String: $reversed");
-}
 void main(List<String> args) {
   print("Please enter a string to reverse:");
   String input = stdin.readLineSync()!;
   reverseString(input);
+}
+
+void reverseString(String input) {
+  // String reversed = input.split('').reversed.join('');
+  String reversed = '';
+  for (int i = input.length - 1; i >= 0; i--) {
+    reversed += input[i];
+  }
+  // print("Reversed String: $reversed");
+  isPalindrome(input, reversed);
+}
+
+void isPalindrome(String input, String reversed) {
+  if (input == reversed) {
+    print("The string is a palindrome.");
+    // return true;
+  } else {
+    print("The string is not a palindrome.");
+    // return false;
+  }
 }
